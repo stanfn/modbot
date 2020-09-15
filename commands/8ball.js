10eb697edbfc2c440a26e077a34ba10d6527ae14
stan <57991726+stanfn@users.noreply.github.com>
2020-09-15 08:40:11 +0700

    Update 8ball.js

--- commands/8ball.js
+++ commands/8ball.js
@@ -1,1 +1,19 @@
+const Discord = require('discord.js')
 
+module.exports.run = async (client, message, args) => {
+
+    message.delete(4500)
+
+    let args2 = args.join(" ")
+    if (!args2.length) {
+		return message.channel.send(`No question provided!`).then(msg => msg.delete(4500))
+	}
+let ball = ["It is certain.","It is decidedly so.","Without a doubt.","Yes – definitely.","You may rely on it."," As I see it, yes.","My reply is no.","My sources say no.","Outlook not so good.","Reply hazy, try again.","Very doubtful.","Signs point to yes."]
+
+let embed = new Discord.RichEmbed()
+.setAuthor('Magic 8Ball', 'https://magic-8ball.com/assets/images/magicBallStart.png')
+.setDescription(ball[Math.floor(Math.random() * parseInt(12))])
+.setColor('RANDOM')
+await message.channel.send(embed).then(msg => msg.delete(5000))
+
+}
